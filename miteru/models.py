d2a8c63c7827db6b6d3773d865ead2298e74c2ff
yosida95 <kohei@yosida95.com>
2013-03-06 05:25:21 +0900

    ツイート長の計算でコメント・タイトルの中のURLを踏まえるように修正

--- miteru/models.py
+++ miteru/models.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 
+import re
+
 from mongoengine import (
     BooleanField,
     connect,
@@ -9,6 +11,7 @@ from mongoengine import (
     ReferenceField,
     StringField,
 )
+
 from .constants import MONGO_HOST
 
 connect('miteru', host=MONGO_HOST, port=27017)
@@ -27,3 +30,85 @@ class Token(Document):
     domain = StringField()
     expiration = DateTimeField()
     available = BooleanField(default=True)
+
+
+class PostBuilder(object):
+    pattern = ur'((?<![\w])(https?://)?\w+([\w\-]+\w)?(\.\w+([\w\-]+\w)?)+'\
+        + ur'(/([\w\.\-\$&%/:=#~!]*\??[\w\.\-\$&%/:=#~!]*[\w&/=#])?)?)'
+
+    def __init__(self, url, title, comment=u''):
+        self.url = url
+        self.title = title
+        self.comment = comment
+
+    def _get_shortened_url_length(self, url):
+        return 23 if url.startswith(u'https') else 22
+
+    def _get_text_length(self, text):
+        length = len(text)
+        for url in re.findall(self.pattern, text):
+            length += self._get_shortened_url_length(url[0]) - len(url[0])
+
+        return length
+
+    def _round_text(self, text, length):
+        matches = [url[0] for url in re.findall(self.pattern, text)]
+        if matches and self._get_text_length(text) > length:
+            deviation = 0
+            result = u''
+            index = 0
+
+            for url in matches:
+                index += text[index:].index(url)
+
+                if index + deviation + self._get_shortened_url_length(url)\
+                        >= length:
+                    break
+
+                deviation += self._get_shortened_url_length(url) - len(url)
+            else:
+                index = len(text)
+
+            result = text[:index][:length - deviation]
+
+            if len(result) + deviation is length:
+                return u'%s…' % result[:-1]
+            else:
+                return u'%s…' % result
+        else:
+            return text[:length]
+
+    def build(self):
+        if len(self.title) > 0 and len(self.comment) > 0:
+            base = u'%s - %s : %s #miteru'
+            assert self._get_text_length(self.comment)\
+                <= 143 - len(base) - self._get_shortened_url_length(self.url)
+
+            text = base % (
+                self.comment,
+                self._round_text(
+                    self.title,
+                    146 - len(base) - self._get_text_length(self.comment)
+                    - self._get_shortened_url_length(self.url)
+                ),
+                self.url
+            )
+        elif len(self.title) > 0:
+            base = u'%s : %s #miteru'
+            text = base % (
+                self._round_text(
+                    self.title,
+                    144 - len(base) - self._get_shortened_url_length(self.url)
+                ),
+                self.url
+            )
+        elif len(self.comment) > 0:
+            base = u'%s - %s #miteru'
+            assert self._get_text_length(self.comment)\
+                <= 144 - len(base) - self._get_shortened_url_length(self.url)
+
+            text = u'%s - %s #miteru' % (self.comment, self.url)
+        else:
+            text = u'%s #miteru' % self.url
+
+        return text
--- miteru/tests.py
+++ miteru/tests.py
@@ -1,21 +1,102 @@
+#-*- coding: utf-8 -*-
+
 import unittest
+
 from pyramid import testing
 from pyramid.i18n import TranslationStringFactory
 
 _ = TranslationStringFactory('miteru')
 
 
-class ViewTests(unittest.TestCase):
+class PostBuilderTest(unittest.TestCase):
 
-    def setUp(self):
-        testing.setUp()
-        
-    def tearDown(self):
-        testing.tearDown()
+    def _get_target_instance(self, *args, **kwargs):
+        from .models import PostBuilder
+        return PostBuilder(*args, **kwargs)
 
-    def test_my_view(self):
-        from miteru.views import my_view
-        request = testing.DummyRequest()
-        response = my_view(request)
-        self.assertEqual(response['project'], 'miteru')
+    def test_get_shortened_url_length(self):
+        inst = self._get_target_instance(
+            u'http://example.com/', u'example', u'comment'
+        )
 
+        self.assertEqual(
+            inst._get_shortened_url_length(u'http://www.google.com/'), 22
+        )
+        self.assertEqual(
+            inst._get_shortened_url_length(u'https://www.google.com/'), 23
+        )
+        self.assertEqual(
+            inst._get_shortened_url_length(u'www.google.com'), 22
+        )
+
+    def test_get_text_length(self):
+        inst = self._get_target_instance(
+            u'http://example.com/', u'example', u'comment'
+        )
+
+        self.assertEqual(inst._get_text_length(u'hello'), 5)
+        self.assertEqual(
+            inst._get_text_length(u'hello http://www.google.com/'), 28
+        )
+        self.assertEqual(
+            inst._get_text_length(u'hello https://www.google.com/'), 29
+        )
+        self.assertEqual(
+            inst._get_text_length(u'hello http://www.google.com/hello'), 28
+        )
+        self.assertEqual(inst._get_text_length(
+            u'http://www.google.com/ http://wwww.yahoo.com/'
+        ), 45)
+
+    def test_round_text(self):
+        inst = self._get_target_instance(
+            u'http://example.com/', u'example', u'comment'
+        )
+        check = lambda length, correct: self.assertEqual(inst._round_text(
+            u'abcdefg http://example.com/ hijklmn https://example.com/', length
+        ), correct)
+
+        check(5, u'abcd…')
+        check(30, u'abcdefg …')
+        check(38, u'abcdefg http://example.com/ hijklm…')
+        check(61, u'abcdefg http://example.com/ hijklmn …')
+        check(62, u'abcdefg http://example.com/ hijklmn https://example.com/')
+        check(100, u'abcdefg http://example.com/ hijklmn https://example.com/')
+
+    def test_build(self):
+        inst = self._get_target_instance(
+            u'http://example.com/', u'example', u'comment'
+        )
+        self.assertEqual(
+            inst.build(), u'comment - example : http://example.com/ #miteru'
+        )
+
+        inst = self._get_target_instance(
+            u'http://example.com/', u'example', u'comment' * 14
+        )
+        self.assertEqual(
+            inst.build(), u'%s - exampl : http://example.com/ #miteru' % (
+                u'comment' * 14
+            )
+        )
+        self.assertEqual(len(inst.build()), 137)
+
+        inst = self._get_target_instance(
+            u'http://example.com/', u'example' * 20
+        )
+        self.assertEqual(
+            inst.build(), u'%s : http://example.com/ #miteru' % (
+                u'example' * 15 + u'ex'
+            )
+        )
+        self.assertEqual(len(inst.build()), 137)
+
+        inst = self._get_target_instance(
+            u'http://example.com/', u'', u'comment'
+        )
+        self.assertEqual(inst.build(), u'comment - http://example.com/ #miteru')
+
+        inst = self._get_target_instance(
+            u'http://example.com/', u'', u''
+        )
+        self.assertEqual(inst.build(), u'http://example.com/ #miteru')
